85d6b18d970b1bf406087cb541893de999fe8f01
ArvinCS <arvinciu86@gmail.com>
2025-03-18 15:46:46 +0700

    style: formatting

--- judgels-backends/judgels-server-api/src/main/java/judgels/uriel/api/contest/Contest.java
+++ judgels-backends/judgels-server-api/src/main/java/judgels/uriel/api/contest/Contest.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Optional;
-
 import org.immutables.value.Value;
 
 @Value.Immutable
