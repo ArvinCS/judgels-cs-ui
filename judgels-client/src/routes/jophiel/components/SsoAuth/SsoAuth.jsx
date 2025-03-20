c3ba422f4fa93e19b6fa6a8b8e5f708498d9f7fe
ArvinCS <arvinciu86@gmail.com>
2025-03-20 19:24:36 +0700

    fix: sso login sometimes not working

--- judgels-client/src/routes/jophiel/components/SsoAuth/SsoAuth.jsx
+++ judgels-client/src/routes/jophiel/components/SsoAuth/SsoAuth.jsx
@@ -10,23 +10,34 @@ class SsoAuth extends Component {
     isAuthorizing: false,
   };
 
+  componentDidMount() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const service = urlParams.get('service');
+    const ticket = urlParams.get('ticket');
+  
+    const { protocol, hostname, port, pathname } = window.location;
+    const baseUrl = `${protocol}//${hostname}${port ? `:${port}` : ''}${pathname}`;
+  
+    if (ticket) {
+      this.handleLoginWithTicket(ticket, service || baseUrl, urlParams);
+    }
+  }
+
+  handleLoginWithTicket = async (ticket, service, urlParams) => {
+    try {
+      await this.props.onLogIn({
+        ticket: ticket,
+        serviceUrl: service,
+      });
+    } catch (error) {
+      urlParams.delete('ticket');
+      const newQuery = urlParams.toString();
+      const newUrl = window.location.pathname + (newQuery ? '?' + newQuery : '');
+      window.history.replaceState({}, '', newUrl);
+    }
+  };
+
   render() {
-    useEffect(async () => {
-      const urlParams = new URLSearchParams(window.location.search);
-      const service = urlParams.get('service');
-      const ticket = urlParams.get('ticket');
-
-      const { protocol, hostname, port, pathname } = window.location;
-      const baseUrl = `${protocol}//${hostname}${port ? `:${port}` : ''}${pathname}`;
-
-      if (ticket) {
-        await this.props.onLogIn({
-          ticket: ticket,
-          serviceUrl: service || baseUrl,
-        });
-      }
-    }, []);
-
     return (
       <button className="sso-auth" onClick={this.logIn} disabled={this.state.isAuthorizing}>
         <img src="/logos/makara_ui.png" alt="Makara UI" className="makara" />
